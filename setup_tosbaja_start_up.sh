e784ca73a68b3e216a04281d9dcd488339e6b8f1
canAktas <canaktas.f@gmail.com>
2023-06-14 20:25:10 +0300

    Small fix

--- setup_tosbaja_start_up.sh
+++ setup_tosbaja_start_up.sh
@@ -7,6 +7,6 @@ sudo chmod 744 /usr/local/bin/tosbaja_startup.sh
 sudo chmod 664 /etc/systemd/system/tosbaja_startup.service
 
 sudo systemctl daemon-reload
-sudo systemctl enable disk-space-check.service
+sudo systemctl enable tosbaja_startup.service
 
 sudo rm setup_tosbaja_start_up.sh

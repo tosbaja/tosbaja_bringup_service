c5392257634bc3cc0fdb67ef090fd89a5a28024b
canAktas <canaktas.f@gmail.com>
2023-06-14 19:58:50 +0300

    init

---
+++ setup_tosbaja_start_up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo mv tosbaja_startup.service /etc/systemd/system/
+sudo mv tosbaja_startup.sh /usr/local/bin/
+
+sudo chmod 744 /usr/local/bin/tosbaja_startup.sh
+sudo chmod 664 /etc/systemd/system/tosbaja_startup.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable disk-space-check.service
+
+sudo rm setup_tosbaja_start_up.sh

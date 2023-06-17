c27772738362ab9cd855c62be7eb1b90a7c4e57e
emrekuru97 <emre.kuru97@hotmail.com>
2023-06-18 00:33:05 +0300

    unvisible topic problem and high resource consuming process behavior fix

--- setup_tosbaja_start_up.sh
+++ setup_tosbaja_start_up.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+echo "Enter the absolte path of your workspace:"
+read ws_path
+
+if [ ${ws_path: -1} = "/" ] 
+then
+    ws_path=${ws_path::-1}
+fi
+
+if [ ${ws_path:0:1} = "/" ]
+then
+    ws_path=${ws_path:1}
+fi
+
+echo $ws_path
+cp tosbaja_startup_template.service tosbaja_startup.service
+service_file="tosbaja_startup.service"
+sed -i "s;current_user;$USER;" $service_file
+sed -i "s;ros_distro;$ROS_DISTRO;" $service_file
+sed -i "s;work_space_path;$ws_path;" $service_file
+sed -i "s;ros_domain_id;$ROS_DOMAIN_ID;" $service_file
+
 sudo cp tosbaja_startup.service /etc/systemd/system/
 sudo cp tosbaja_startup.sh /usr/local/bin/
 
@@ -8,3 +29,4 @@ sudo chmod 664 /etc/systemd/system/tosbaja_startup.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable tosbaja_startup.service
+rm tosbaja_startup.service
--- tosbaja_startup.sh
+++ tosbaja_startup.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
-source /opt/ros/humble/setup.bash
-source /home/gna/ws/install/setup.bash
+
+waitForNetworkToGetAnIp(){
+  while true
+  do
+    sleep 1
+    string=$(ip -f inet addr show $2)
+
+    if [ "$string" = "$1" ]; then
+        echo "No ip address was found."
+    else
+        echo "Succesfully got an ip."
+        break
+    fi
+done
+}
+
+# TO-DO get network adapter name from enviroment in this case wlo1
+waitForNetworkToGetAnIp "" "wlo1"
+
+source /opt/ros/$ROS_DISTRO/setup.bash
+source $WORK_SPACE_PATH/install/setup.bash
+ros2 daemon stop
+ros2 daemon start
 ros2 launch start_tosbaja rsp.launch.py
